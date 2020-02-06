13f9ec2bc7ba2b338c963746c6d781f1ce138b1a
HenryWasThere <58852236+HenryWasThere@users.noreply.github.com>
2020-02-06 23:12:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -59,21 +59,6 @@ if (!config.EMAIL_FROM) { //sending email
 if (!config.EMAIL_TO) { //sending email
     throw new Error('missing EMAIL_TO');
 }
-if (!config.WEATHER_API_KEY) { //weather api key
-    throw new Error('missing WEATHER_API_KEY');
-}
-if (!config.PG_CONFIG) { //pg config
-    throw new Error('missing PG_CONFIG');
-}
-if (!config.FB_APP_ID) { //app id
-    throw new Error('missing FB_APP_ID');
-}
-if (!config.ADMIN_ID) { //admin id for login
-    throw new Error('missing ADMIN_ID');
-}
-if (!config.FB_PAGE_INBOX_ID) { //page inbox id - the receiver app
-    throw new Error('missing FB_PAGE_INBOX_ID');
-}
 
 app.set('port', (process.env.PORT || 5000))
 
